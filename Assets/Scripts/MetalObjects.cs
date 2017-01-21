a509f20ce7e6da4dfff6800d5c30049856bee5f8
Stewart L. McCready <stewmcc@gmail.com>
2017-01-21 02:54:11 +0000

    Basic Chargeable Microwave, from metal items.

---
+++ Assets/Scripts/MetalObjects.cs
@@ -0,0 +1,19 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+using VRTK;
+
+public class MetalObjects : VRTK_InteractableObject {
+	[Tooltip("How much the object will power up the microwave bye")]
+	[SerializeField]
+	float metalStrength = 1.0f;
+
+	private void Start() {
+		gameObject.tag = "MetalObject";
+	}
+
+	public float MetalStrength() {
+		return metalStrength;
+	}
+}
--- Assets/Scripts/MicroWave.cs
+++ Assets/Scripts/MicroWave.cs
@@ -1,9 +1,66 @@
-﻿using System.Collections;
-using System.Collections.Generic;
+﻿
+using System;
 using UnityEngine;
+using UnityEngine.UI;
 using VRTK;
 
 public class MicroWave : VRTK_InteractableObject {
 
-	
+	[Tooltip("Text to show number countdown of timer.")]
+	[SerializeField]
+	private Text countdownText = null;
+
+	[Tooltip("Image used radial fill for countdown.")]
+	[SerializeField]
+	private Image countDownImage =null;
+
+	[SerializeField]
+	private float damageAmount = 10.0f;
+
+	float chargeTime_ = 6.0f;
+	float timeRemaining_ = 0.0f;
+	bool isCharging_ =false;
+		
+	protected override void Awake() {
+		base.Awake();
+		timeRemaining_ = chargeTime_;
+		ResetClock();
+	}
+
+	protected void OnCollisionEnter(Collision collision) {
+		if(collision.gameObject.tag == "MetalObject") {
+			isCharging_ = true;
+			collision.gameObject.GetComponent<MetalObjects>().MetalStrength();
+			Destroy(collision.gameObject);
+			// get metal value, and calculate charge time.
+			// or use metal value for damage calculation
+		}
+	}
+		
+	protected override void Update() {
+		base.Update();
+		// check if it can charge
+		if (isCharging_) {
+			// run countdown
+			timeRemaining_ -= Time.deltaTime;
+			if (timeRemaining_ < 0) {
+				timeRemaining_ = chargeTime_;
+				isCharging_ = false;
+				FireMicroWave();
+				ResetClock();
+			}
+			else {				
+				countdownText.text = timeRemaining_.ToString("F1");
+				countDownImage.fillAmount = timeRemaining_ / chargeTime_;
+			}
+		}
+	}
+
+	private void FireMicroWave() {
+
+	}
+	private void ResetClock() {
+		countdownText.text = "00:00";
+		countDownImage.fillAmount = 0.0f;
+	}
 }
