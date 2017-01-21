f0f5a7e48e94589df4ab511b6e4884368c055df4
Stewart L. McCready <stewmcc@gmail.com>
2017-01-21 04:40:12 +0000

    Example Ai Navigation Via NavMesh

---
+++ Assets/Scripts/EnemyNavigation.cs
@@ -0,0 +1,18 @@
+﻿using UnityEngine;
+using UnityEngine.AI;
+
+public class EnemyNavigation : MonoBehaviour {
+
+	private Transform target_ =null;
+	private NavMeshAgent agent_ = null;
+	void Start() {
+		agent_ = GetComponent<NavMeshAgent>();
+		target_ = GameObject.FindWithTag("MainCamera").transform;
+		agent_.destination = target_.position;		
+	}
+	private void Update() {
+		if (agent_.remainingDistance <= float.Epsilon) {
+			agent_.destination = target_.position;
+		}
+	}
+}
