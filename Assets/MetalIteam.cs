c1014078ddab0fbc3bcd635759222ad50afb8e14
Alan Yeats <alan@alanyeats.com>
2017-01-21 01:18:59 +0000

    Added a base metal Iteam.
    Base metal iteam can be put in a microwave to fun

---
+++ Assets/MetalIteam.cs
@@ -0,0 +1,36 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class MetalIteam : MonoBehaviour {
+
+
+    [Tooltip("The amount of metal level the iteam is ")]
+    [SerializeField]
+    protected int metalAmmount = 10;
+
+
+    [Tooltip("The chance of spawn.")]
+    [Range(0,100)]
+    [SerializeField]
+    protected int rarityOfSpawn = 10;
+
+
+    public int MetalAmmount()
+    {
+        return metalAmmount;
+    }
+    public int RarityOfSpawn()
+    {
+        return rarityOfSpawn;
+    }
+    // Use this for initialization
+    void Start () {
+		
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		
+	}
+}
