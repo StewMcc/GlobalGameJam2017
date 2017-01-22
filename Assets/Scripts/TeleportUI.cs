3f9d5614be8aa09f5df010aad3ec71d46295f818
Stewart L. McCready <stewmcc@gmail.com>
2017-01-22 03:21:39 +0000

    Added telporter and time down

---
+++ Assets/Scripts/TeleportUI.cs
@@ -0,0 +1,25 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+using VRTK;
+public class TeleportUI : MonoBehaviour {
+
+	[SerializeField]
+	private Image telportStateImage;
+
+	[SerializeField]
+	private VRTK_BasePointer baseTelporter;
+
+
+	// Update is called once per frame
+	void Update () {
+
+		if (baseTelporter.CanActivate()) {
+			telportStateImage.color = Color.green;
+		}else {
+			telportStateImage.color = Color.red;
+
+		}
+	}
+}
