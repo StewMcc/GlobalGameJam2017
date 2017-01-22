a5cd4d5f87cae1b694c793a886671dd5cbf9bc42
Stewart L. McCready <stewmcc@gmail.com>
2017-01-22 00:32:36 +0000

    Updated Assets

--- Assets/Scripts/WaveController.cs
+++ Assets/Scripts/WaveController.cs
@@ -3,25 +3,25 @@ using UnityEngine.UI;
 
 [System.Serializable]
 public class WaveSettings {
-    public float enemySpeedMultiplier = 1.0f;
+	public float enemySpeedMultiplier = 1.0f;
 
-    public int increaseOfEnemies = 1;
+	public int increaseOfEnemies = 1;
 
-    public float enemyScoreMultiplier = 1.0f;
+	public float enemyScoreMultiplier = 1.0f;
 	public float enemyDamageMultiplier = 1.0f;
 
 	public float waveDelay = 1.0f;
 
-    public EnemySpawner[] enemySpawners;
+	public EnemySpawner[] enemySpawners;
 }
 
 public class WaveController : MonoBehaviour {
-    [SerializeField]
-    Text waveText = null;
-    [SerializeField]
-    WaveSettings[] waveSettings = null;
+	[SerializeField]
+	Text waveText = null;
+	[SerializeField]
+	WaveSettings[] waveSettings = null;
 
-    int waveNumber = 0;
+	int waveNumber = 0;
 
 	SimpleTimer waveTimer = new SimpleTimer();
 
@@ -54,11 +54,11 @@ public class WaveController : MonoBehaviour {
 		}
 	}
 
-    private void SpawnWave() {        
-        numberOfEnemiesLeft = 0;
-        foreach (EnemySpawner spawner in waveSettings[waveNumber].enemySpawners) {
-            numberOfEnemiesLeft += spawner.StartNewWave(waveSettings[waveNumber]);			
-        }		
+	private void SpawnWave() {        
+		numberOfEnemiesLeft = 0;
+		foreach (EnemySpawner spawner in waveSettings[waveNumber].enemySpawners) {
+			numberOfEnemiesLeft += spawner.StartNewWave(waveSettings[waveNumber]);			
+		}		
 		waveNumber++;
 		if (waveNumber < waveSettings.Length) {
 			waveTimer.SetTimer(waveSettings[waveNumber].waveDelay);
